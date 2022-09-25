602fea10fb3ba045eaff16e7d9c72778abfcc48d
Mitesh Vora <miteshvora18@gmail.com>
2022-09-25 13:56:54 +0530

    Added new React files

---
+++ src/components/VideoItem.js
@@ -0,0 +1,15 @@
+
+const VideoItem =({video, onVideoSelect}) => {
+    return (
+        <div onClick={() => onVideoSelect(video)} className="video-item item">
+            <img className="ui image" alt={video.snippet.title} key={video.id.videoId} src={video.snippet.thumbnails.medium.url} />
+            <div className="content">
+                <div className="header">
+                    {video.snippet.title}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default VideoItem;
